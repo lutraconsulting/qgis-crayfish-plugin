a46b28cde4a1501ecd08e874d1212e0e7768e144
Martin Dobias <wonder.sk@gmail.com>
2014-11-06 18:01:11 +0700

    Small updates to About dialog + new logo

--- qgis_plugin/crayfish_about_dialog_widget.py
+++ qgis_plugin/crayfish_about_dialog_widget.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'crayfish_about_dialog_widget.ui'
 #
-# Created: Sat Sep 21 22:14:33 2013
-#      by: PyQt4 UI code generator 4.9.1
+# Created: Thu Nov  6 17:59:20 2014
+#      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,12 +12,21 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(400, 252)
+        Dialog.resize(518, 306)
         self.gridLayout = QtGui.QGridLayout(Dialog)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.horizontalLayout = QtGui.QHBoxLayout()
@@ -42,7 +51,7 @@ class Ui_Dialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.lutraLogoLabel.sizePolicy().hasHeightForWidth())
         self.lutraLogoLabel.setSizePolicy(sizePolicy)
-        self.lutraLogoLabel.setMinimumSize(QtCore.QSize(128, 46))
+        self.lutraLogoLabel.setMinimumSize(QtCore.QSize(200, 69))
         self.lutraLogoLabel.setText(_fromUtf8(""))
         self.lutraLogoLabel.setObjectName(_fromUtf8("lutraLogoLabel"))
         self.verticalLayout.addWidget(self.lutraLogoLabel)
@@ -66,12 +75,5 @@ class Ui_Dialog(object):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "About Crayfish", None, QtGui.QApplication.UnicodeUTF8))
-        self.aboutBrowser.setHtml(QtGui.QApplication.translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal;\" bgcolor=\"#efe1bb\">\n"
-"<p style=\" margin-top:18px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'FreeSans,Geneva,Arial,sans-serif\'; font-size:8pt; font-weight:600; color:#412824;\">Crayfish Plugin</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'FreeSans,Geneva,Arial,sans-serif\'; font-size:8pt; color:#412824;\">The Crayfish Plugin is a collection of tools for hydraulic modellers working with TUFLOW and other modelling packages. It aims to use QGIS as an efficient and effective pre and post-processor.</span></p>\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'FreeSans,Geneva,Arial,sans-serif\'; font-size:8pt; color:#412824;\">Check out the </span><a href=\"http://www.lutraconsulting.co.uk/resources/crayfish\"><span style=\" font-family:\'FreeSans,Geneva,Arial,sans-serif\'; font-size:8pt; text-decoration: underline; color:#412824;\">Crayfish resources page</span></a><span style=\" font-family:\'FreeSans,Geneva,Arial,sans-serif\'; font-size:8pt; color:#412824;\"> on our website for more information.</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        Dialog.setWindowTitle(_translate("Dialog", "About Crayfish", None))
 
