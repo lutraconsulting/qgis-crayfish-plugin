6b1657a8476fc75da6f4fd031bc647940141a661
Martin Dobias <wonder.sk@gmail.com>
2015-03-17 17:15:07 +0700

    Added few basic scripts using crayfish core library

---
+++ tools/cf_info.py
@@ -0,0 +1,18 @@
+
+import sys
+sys.path.append('../plugin')
+import crayfish
+
+if len(sys.argv) == 1:
+  print "Syntax: %s <2dm file>" % sys.argv[0]
+  sys.exit(1)
+
+try:
+  m = crayfish.Mesh(sys.argv[1])
+except ValueError:
+  print "Failed to load mesh in %s" % sys.argv[1]
+  sys.exit(1)
+
+
+print "Nodes:    %7d" % m.node_count()
+print "Elements: %7d" % m.element_count()
