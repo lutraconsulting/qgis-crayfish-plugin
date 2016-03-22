249e637fbbe41d3b9ff4970fc16b01bb36843e6c
Peter Petrik <zilolv@gmail.com>
2016-03-22 12:20:39 +0100

    fix script

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -8,37 +8,24 @@
 #                                                                         #
 ###########################################################################
 
-ASTYLEOPTS="--preserve-date
---indent-preprocessor
---brackets=break
---convert-tabs
---indent=spaces=2
---indent-classes
---indent-labels
---indent-namespaces
---indent-switches
---one-line=keep-blocks
---max-instatement-indent=40
---min-conditional-indent=-1
---suffix=none
---pad=oper
---pad=paren-in
---unpad=paren"
-
 set -e
 
 ##### PYTHON
 
-PYFILES=find .. -type f \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \)
+cd ../plugin
+PYFILES=$(find . -type f -name "*.py" --max-depth=1)
 for i in $PYFILES; do
     echo "$i"
     autopep8 --in-place --ignore=E261,E265,E402,E501 "$i"
 done
 
 ##### C++
-CFILES=find .. -type f \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \)
+cd ../corelib
+CFILES=$(find . -type f \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \))
 
 for i in ../**/*.cpp; do
     echo "$i"
     astyle --style=google --convert-tabs --indent=spaces=2 --preserve-date --suffix=none "$i"
 done
+
+cd ../scripts
