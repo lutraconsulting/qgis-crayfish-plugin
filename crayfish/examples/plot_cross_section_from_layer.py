a3f4bfdf343deb97f55deb15bdd2814d5f7897a9
Martin Dobias <wonder.sk@gmail.com>
2016-04-18 17:40:52 +0800

    Change the example to use shapefile instead of memory provider

--- crayfish/examples/plot_cross_section_from_layer.py
+++ crayfish/examples/plot_cross_section_from_layer.py
@@ -7,7 +7,7 @@ from qgis.core import QgsGeometry, QgsPoint, QgsFeature, QgsVectorLayer
 
 import crayfish.plot
 
-from . import load_example_mesh
+from . import load_example_mesh, my_cross_section_layer
 
 # Load mesh and a results file
 mesh = load_example_mesh()
@@ -15,16 +15,10 @@ mesh = load_example_mesh()
 # Get data for the plot
 output = mesh.dataset(1).output(0)  # first output of second dataset
 
-# Prepare a memory layer
-layer = QgsVectorLayer("LineString", "my layer", "memory")
-f = QgsFeature()
-f.setGeometry(QgsGeometry.fromPolyline([QgsPoint(1000,1005), QgsPoint(1028,1005)]))
-layer.dataProvider().addFeatures([f])
-
-# -or- load a shapefile layer (instead of using temporary memory layer)
-#
-# layer = QgsVectorLayer("/data/shapefile.shp", "my layer", "ogr")
-# if not layer.isValid(): raise Exception("Load failed!")
+# Load a shapefile layer (instead of using temporary memory layer)
+layer = QgsVectorLayer(my_cross_section_layer, "my layer", "ogr")
+if not layer.isValid():
+    raise Exception("Load failed!")
 
 for feature in layer.getFeatures():
 
