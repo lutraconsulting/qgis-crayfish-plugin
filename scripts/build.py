a6742224a1949311d167bb0a08ad8d50461b22cc
Peter Petrik <zilolv@gmail.com>
2016-07-11 12:41:20 +0200

    separate build info to a file for reusability

--- scripts/build.py
+++ scripts/build.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import sys
+import argparse
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 cr_dir = os.path.join(this_dir, os.pardir, "crayfish")
@@ -9,5 +10,11 @@ sys.path.insert(0, cr_dir)
 import buildinfo
 
 ver = buildinfo.plugin_version_str()
-print "Building version " + ver
+plat = buildinfo.findPlatformVersion()
 
+parser = argparse.ArgumentParser(description='Build Tool')
+parser.add_argument('--dst', help='destination directory', action='store', required=True)
+args = parser.parse_args()
+
+print "Building version " + ver + " for " + plat
+print "Destination directory " + args.dst
