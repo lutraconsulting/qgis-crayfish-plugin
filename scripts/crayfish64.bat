50540122015869ee62c3bf8c498fbe95528b03a5
Saber <saber.razmjooei@lutraconsulting.co.uk>
2017-09-20 14:30:53 +0100

    Added instruction for MS VS 2015. I am not sure about qmake -spec win32-msvc2012

--- scripts/crayfish64.bat
+++ scripts/crayfish64.bat
@@ -1,10 +1,11 @@
-
 call c:\osgeo4w64\osgeo4w.bat echo OSGEO4W64
-call vs2010_64bit.bat
+# call vs2010_64bit.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+path %path%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
 
 cd ..\corelib
 nmake distclean
-qmake -spec win32-msvc2010 "CONFIG+=release"
+qmake -spec win32-msvc2012 "CONFIG+=release"
 nmake
 
 set /p CFVER=Crayfish package version:
