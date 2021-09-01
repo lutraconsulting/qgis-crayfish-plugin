70d494cbe37e7097aa3ea1d5b10e9faad3d27052
PeterPetrik <zilolv@gmail.com>
2021-09-01 11:54:06 +0200

    0122

--- crayfish/plot.py
+++ crayfish/plot.py
@@ -36,13 +36,8 @@ try:
     import pyqtgraph as pg
     from pyqtgraph.exporters import ImageExporter
 except ImportError:
-    import sys
-    import os
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-    path = os.path.join(this_dir, 'pyqtgraph-0.11.1-py2.py3-none-any.whl')
-    sys.path.append(path)
-    import pyqtgraph as pg
-    from pyqtgraph.exporters import ImageExporter
+    import crayfish.pyqtgraph_0_12_2 as pg
+    from crayfish.pyqtgraph_0_12_2.exporters import ImageExporter
 
 from .utils import integrate
 
